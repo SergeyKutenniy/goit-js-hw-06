f8e078ffcd2ad82a75c12e99a9b9c582956c4a47
Sergey Kutenniy <kutenniysergey@gmail.com>
2022-02-03 17:40:09 +0200

    task2-done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const elemLi = document.createElement("li");
+  elemLi.textContent = ingredients[i];
+  elemLi.classList.add("item");
+  ingredientsEl.append(elemLi);
+}
