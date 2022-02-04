00ce807cf1084cb45afc49923a7526556b6dfc07
Sergey Kutenniy <kutenniysergey@gmail.com>
2022-02-04 22:34:24 +0200

    task4-done

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const btnIncrement = document.querySelector("[data-action=increment]");
+const btnDecrement = document.querySelector("[data-action=decrement]");
+const value = document.querySelector("#value");
+
+let counterValue = 0;
+
+const render = () => {
+  value.textContent = counterValue.toString();
+};
+
+btnIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  render();
+});
+
+btnDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  render();
+});
+
+render();
