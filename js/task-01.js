b0134f3557eba7e195fc71b16802d5c07cd32177
Sergey Kutenniy <kutenniysergey@gmail.com>
2022-02-07 13:54:07 +0200

    task1-done

--- js/task-01.js
+++ js/task-01.js
@@ -1,28 +1,9 @@
-// Выводит количество груп #categories
+// Выводит название группы и количесто подкатегорий
 
-const elCategories = categories.querySelectorAll(".item");
-console.log("Number of categories: ", elCategories.length);
+const categoriesEl = document.querySelectorAll(".item");
+console.log("Number of categories ", categoriesEl.length);
 
-// Выводит количество подкатегорий группы "Animal"
-
-const groupAnimals = categories.querySelector(".item");
-console.log("Category: ", groupAnimals.firstElementChild.textContent);
-
-const typeGroupAnimals = groupAnimals.querySelectorAll("li");
-console.log("Elements: ", typeGroupAnimals.length);
-
-// Выводит количество подкатегорий группы "Products"
-
-const groupProducts = categories.querySelectorAll(".item");
-console.log("Category: ", groupProducts[1].firstElementChild.textContent);
-
-const typeGroupProducts = groupProducts[1].querySelectorAll("li");
-console.log("Elements: ", typeGroupProducts.length);
-
-// Выводит количество подкатегорий группы "Technologies"
-
-const groupTechnologies = categories.querySelectorAll(".item");
-console.log("Category: ", groupTechnologies[2].firstElementChild.textContent);
-
-const typeGroupTechnologies = groupTechnologies[2].querySelectorAll("li");
-console.log("Elements: ", typeGroupTechnologies.length);
+categoriesEl.forEach(function (category) {
+  console.log("Category: ", category.firstElementChild.textContent);
+  console.log("Element: ", category.lastElementChild.children.length);
+});
